003f4768edc734574b9fd0abd078bef254e64c92
Arthur Apanovics <arthur.apanovics@gmail.com>
2023-02-13 13:07:12 +1300

    Add unit test project

---
+++ VirtualVendingMachineUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace VirtualVendingMachineUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
