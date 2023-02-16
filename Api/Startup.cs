3131d7e999088fa7fbefcac36893da937917e025
Arthur Apanovics <arthur.apanovics@gmail.com>
2023-02-16 20:14:21 +1300

    Auto-format startup

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -23,7 +23,17 @@ namespace VirtualVendingMachine
         {
             services.AddControllers();
             services.AddSwaggerGen(
-                c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Virtual Vending Machine API", Version = "v1" }); }
+                c =>
+                {
+                    c.SwaggerDoc(
+                        "v1",
+                        new OpenApiInfo
+                        {
+                            Title = "Virtual Vending Machine API",
+                            Version = "v1"
+                        }
+                    );
+                }
             );
             services.AddMediatR(typeof(Startup));
 
@@ -39,7 +49,12 @@ namespace VirtualVendingMachine
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Virtual Vending Machine API v1"));
+                app.UseSwaggerUI(
+                    c => c.SwaggerEndpoint(
+                        "/swagger/v1/swagger.json",
+                        "Virtual Vending Machine API v1"
+                    )
+                );
             }
 
             app.UseHttpsRedirection();
