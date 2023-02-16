8f57f9542f29b3cc4e0eb3683aaa84be08d8a76a
Arthur Apanovics <arthur.apanovics@gmail.com>
2023-02-16 19:43:57 +1300

    Rename ASP project

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -23,7 +23,7 @@ namespace VirtualVendingMachine
         {
             services.AddControllers();
             services.AddSwaggerGen(
-                c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "VirtualVendingMachine", Version = "v1" }); }
+                c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Virtual Vending Machine API", Version = "v1" }); }
             );
             services.AddMediatR(typeof(Startup));
 
@@ -39,7 +39,7 @@ namespace VirtualVendingMachine
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "VirtualVendingMachine v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Virtual Vending Machine API v1"));
             }
 
             app.UseHttpsRedirection();
